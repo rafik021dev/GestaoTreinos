7f779e4510d3c576ed7a913854147011b16a4794
GustavoVFranca <gustavofranca7778@gmail.com>
2025-11-27 17:14:30 -0300

    usuarioDAO pronto

--- GestaoTreinos/src/gestaotreinos/model/dao/UsuarioDAO.java
+++ GestaoTreinos/src/gestaotreinos/model/dao/UsuarioDAO.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.sql.ResultSet;
 import javax.swing.JOptionPane;
 
@@ -24,26 +26,132 @@ public class UsuarioDAO {
 		String sSql = "INSERT INTO usuario "
                 + "(nome, sexo, idade, peso, altura, metapeso, email, senha)"
 				+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-		try (PreparedStatement statement = conn.prepareStatement(sSql)){
-			statement.setString(1, oUsuario.getNome());
-			statement.setString(2,String.valueOf(oUsuario.getSexo()));
-			statement.setInt(3, oUsuario.getIdade());
-			statement.setDouble(4, oUsuario.getPeso());
-			statement.setDouble(5, oUsuario.getAltura());
-			statement.setDouble(6, oUsuario.getMetaPeso());
-			statement.setNull(6, java.sql.Types.DECIMAL);
-			statement.setString(7, oUsuario.getEmail());
-			statement.setString(8, oUsuario.getSenha());
+		try (PreparedStatement stmt = conn.prepareStatement(sSql)){
+			stmt.setString(1, oUsuario.getNome());
+			stmt.setString(2,String.valueOf(oUsuario.getSexo()));
+			stmt.setInt(3, oUsuario.getIdade());
+			stmt.setDouble(4, oUsuario.getPeso());
+			stmt.setDouble(5, oUsuario.getAltura());
+			
+			if(oUsuario.getMetaPeso() != null) {
+				stmt.setDouble(6, oUsuario.getMetaPeso());
+			}
+			else {	
+				stmt.setNull(6, java.sql.Types.DECIMAL);
+			}
+			
+			stmt.setString(7, oUsuario.getEmail());
+			stmt.setString(8, oUsuario.getSenha());
 
-			statement.executeUpdate();
+			stmt.executeUpdate();
 		}
+	}
+	/*
+	 * metodo responsável por atualizar um usuário ja cadastrado no sistema
+	 */
+	public void atualizarUsuario(Usuario oUsuario) throws SQLException{
+		String sSql = "UPDATE usuario SET"
+				+"nome = ?, sexo = ?, idade = ?, peso = ?,"
+				+ " altura = ?, metapeso = ?, email = ?, senha = ? "
+				+"WHERE idusuario = ?";
+		try (PreparedStatement stmt = conn.prepareStatement(sSql)){
+			stmt.setString(1, oUsuario.getNome());
+			stmt.setString(2,String.valueOf(oUsuario.getSexo()));
+			stmt.setInt(3, oUsuario.getIdade());
+			stmt.setDouble(4, oUsuario.getPeso());
+			stmt.setDouble(5, oUsuario.getAltura());
+			
+			if(oUsuario.getMetaPeso() != null) {
+				stmt.setDouble(6, oUsuario.getMetaPeso());
+			}
+			else {	
+				stmt.setNull(6, java.sql.Types.DECIMAL);
+			}
+			
+			stmt.setString(7, oUsuario.getEmail());
+			stmt.setString(8, oUsuario.getSenha());
+			stmt.setLong(9, oUsuario.getIdUsuario());
 
+			stmt.executeUpdate();
+		}
+	}	
+	/*
+	 * metodo responável por deletar um usuário cadastrado no sistema
+	 */
+	public void deletarUsuario(long id) throws SQLException{
+		String sSql = "DELETE FROM usuario WHERE idusuario = ?";
+		
+		try (PreparedStatement stmt = conn.prepareStatement(sSql)){
+			stmt.setLong(1, id);
+			stmt.executeUpdate();
+		}
+	}
+	/* 
+	 * metodo para realizar uma busca por id nos usuários do sistema
+	 */
+	    public Usuario buscarPorId(long id) throws SQLException {
+	        String sql = "SELECT idusuario, nome, sexo, idade, peso, altura, "
+	                   + "metapeso, email, senha FROM usuario WHERE idusuario = ?";
+
+	        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+	            stmt.setLong(1, id);
+	            try (ResultSet rs = stmt.executeQuery()) {
+	                if (rs.next()) {
+	                   Usuario usuarioRS = new Usuario();
+	                   usuarioRS.setIdUsuario(rs.getInt("idusuario"));
+	                   usuarioRS.setNome(rs.getString("nome"));
+	                   usuarioRS.setSexo(rs.getString("sexo").charAt(0));
+	                   usuarioRS.setIdade(rs.getInt("idade"));
+	                   usuarioRS.setIdade(rs.getInt("idade"));
+	                   usuarioRS.setIdade(rs.getInt("idade"));
+	                   usuarioRS.setPeso(rs.getDouble("peso"));
+	                   usuarioRS.setAltura(rs.getDouble("altura"));
+	                   usuarioRS.setMetaPeso(rs.getDouble("metapeso"));
+	                   usuarioRS.setEmail(rs.getString("email"));
+	                   usuarioRS.setSenha(rs.getString("senha"));	 
+	                   
+	                   return usuarioRS;
+	                }
+	            }
+	        }
+	        return null;
+	    }
+	    
+	    /* 
+	     * metodo para listar todos usuários do sistema por ordem de id
+	     */
+	    public List<Usuario> listarUsuarios() throws SQLException {
+	        String sSql = "SELECT idusuario, nome, sexo, idade, peso, altura, "
+	                   + "metapeso, email, senha FROM usuario ORDER BY idusuario";
+	        
+	        List<Usuario> lista = new ArrayList();	
+	
+	        try (PreparedStatement stmt = conn.prepareStatement(sSql);
+	        		ResultSet rs = stmt.executeQuery()) {
+	        			while(rs.next()) {
+	        				 Usuario usuarioRS = new Usuario();
+	  	                   usuarioRS.setIdUsuario(rs.getInt("idusuario"));
+	  	                   usuarioRS.setNome(rs.getString("nome"));
+	  	                   usuarioRS.setSexo(rs.getString("sexo").charAt(0));
+	  	                   usuarioRS.setIdade(rs.getInt("idade"));
+	  	                   usuarioRS.setPeso(rs.getDouble("peso"));
+	  	                   usuarioRS.setAltura(rs.getDouble("altura"));
+	  	                   usuarioRS.setMetaPeso(rs.getDouble("metapeso"));
+	  	                   usuarioRS.setEmail(rs.getString("email"));
+	  	                   usuarioRS.setSenha(rs.getString("senha"));	 
+	  	                   
+	  	                   lista.add(usuarioRS);
+	        			}
+	        		}
+	        return lista;
+	    }
+	    
         /**
          * Método responsável por validar o Login do Usuário no Sistema
          * @param oUsuario
          * @return ResultSet ResSet
          * @todo TESTAR!
-         */
+         
         public ResultSet autenticacaoUsuario(Usuario oUsuario) {
             //conn = new ConexaoBD().//amanhã, adaptar o novo método de conexao passando os parâmetros corretos
         //método não testado, apenas copiei o que tinha feito no NetBeans
@@ -59,7 +167,7 @@ public class UsuarioDAO {
                 statement.setString(1, oUsuario.getNome());
                 statement.setString(2, oUsuario.getSenha());
 
-                ResultSet ResSet = pstm.executeQuery();
+                ResultSet ResSet = statement.executeQuery();
 
                 return ResSet;
 
@@ -68,24 +176,6 @@ public class UsuarioDAO {
 
                 return null;
             }
-        }
-    }
-	
-//		String sql = "INSERT INTO usuario "
-//				+ "(nome, sexo, idade, peso, altura, metapeso, email, senha)"
-//				+ "VALUES ("
-//				+ usuario.getNome()	+" , "
-//				+ usuario.getSexo() +" , "
-//				+ usuario.getIdade() +" , "
-//				+ usuario.getPeso() +" , "
-//				+ usuario.getAltura() +" , "
-//				+ usuario.getMetaPeso() +" , "
-//				+ usuario.getEmail() +" , "
-//				+ usuario.getSenha() + ")";
-//
-//		    try (Statement st = conn.createStatement()) {
-//		        st.executeUpdate(sql);
-//
-//		    }
+        }*/	
 	}
-}
+
--- GestaoTreinos/src/gestaotreinos/model/dao/teste.java
+++ GestaoTreinos/src/gestaotreinos/model/dao/teste.java
@@ -1,33 +1,79 @@
 package gestaotreinos.model.dao;
-import java.sql.Connection;
-import gestaotreinos.model.dao.ConexaoBD;
-import gestaotreinos.model.dao.UsuarioDAO;
+
 import gestaotreinos.model.entity.Usuario;
+import java.sql.Connection;
+import java.util.List;
 
 public class teste {
 
-	public static void main(String[] args) {
-		try {
+    public static void main(String[] args) {
+        Connection conn = null;
+        
+        try {
+            
             ConexaoBD conexaoBD = new ConexaoBD();
-            Connection conn = conexaoBD.connection("GestaoTreinos", "postgres", "07171826");
-
-            Usuario u = new Usuario();
-            u.setNome("jairton3");
-            u.setSexo('M');
-            u.setIdade(18);
-            u.setPeso(80);
-            u.setAltura(1.80);
-            u.setMetaPeso(78.0);
-            u.setEmail("jairto3gmail.com");
-            u.setSenha("1234567");
-
+            conn = conexaoBD.connection("GestaoTreinos", "postgres", "07171826");
+            
             UsuarioDAO usuarioDAO = new UsuarioDAO(conn);
-            usuarioDAO.insert(u);
+            Usuario usuarioTeste = new Usuario();
+            usuarioTeste.setIdUsuario(2);
+            usuarioTeste.setNome("gustavo");
+            usuarioTeste.setSexo('M');
+            usuarioTeste.setIdade(18);
+            usuarioTeste.setPeso(80.0);
+            usuarioTeste.setAltura(1.80);
+            usuarioTeste.setMetaPeso(78.00);
+            usuarioTeste.setEmail("gustavo@email.com");
+            usuarioTeste.setSenha("12345");
+            
+            System.out.println("\nusuario inserido");
 
-            System.out.println("Usuario inserido.");
-
+            List<Usuario> usuarios = usuarioDAO.listarUsuarios();
+            
+            System.out.println("\nqtd usuarios: " + usuarios.size());
+            
+                for (Usuario u : usuarios) {
+                    System.out.println("ID: " + u.getIdUsuario());
+                    System.out.println("Nome: " + u.getNome());
+                    System.out.println("Email: " + u.getEmail());
+                    System.out.println("Peso: " + u.getPeso());
+                    System.out.println("   ");
+                }
+                usuarioDAO.deletarUsuario(2);
+                System.out.println("usuario deletado");
+                
+                List<Usuario> usuarios2 = usuarioDAO.listarUsuarios();
+                
+                System.out.println("\nnumero de usuarios: " + usuarios.size());
+                
+                    for (Usuario u : usuarios) {
+                        System.out.println("ID: " + u.getIdUsuario());
+                        System.out.println("Nome: " + u.getNome());
+                        System.out.println("Email: " + u.getEmail());
+                        System.out.println("Peso: " + u.getPeso());
+                        System.out.println("   ");
+                    }
+                    
+                    Usuario usuario = usuarioDAO.buscarPorId(1); 
+                    System.out.println("busca:");
+                    if (usuario != null) {
+                        System.out.println("ID: " + usuario.getIdUsuario());
+                        System.out.println("Nome: " + usuario.getNome());
+                        System.out.println("Email: " + usuario.getEmail());
+                        System.out.println("Peso: " + usuario.getPeso());
+                    } 
+                    
         } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
             e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }         
         }
     }
 }
